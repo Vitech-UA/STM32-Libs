f63269e12e17a59f5385dc7156ccdb99ad457c6b
Vitech-UA <viktorstarovit@gmail.com>
2020-12-13 14:29:24 +0200

    Додано роботу з PWM DMA Brust mode

--- Моя бібліотека роботи з таймерами/timer_cmsis/timer.c
+++ Моя бібліотека роботи з таймерами/timer_cmsis/timer.c
@@ -7,6 +7,11 @@
 
 #include "timer.h"
 
+uint32_t aSRC_Buffer[BUFFER_DATA_NUMBER] = { 800 - 1, 1, 800, 800 - 1, 0,8500, 4000 - 1, 2, 2000 };
+//uint32_t aSRC_Buffer[BUFFER_DATA_NUMBER] = { 1000, 1, 2000, 1, 3000,1, 4000 ,1, 5000 };
+
+uint16_t Tim1Prescaler = 0;
+
 void DelayUs(uint16_t Delay_Us) {
 
 	uint16_t tim_psc_val = 0;
@@ -18,7 +23,6 @@ void DelayUs(uint16_t Delay_Us) {
 	while (!(TIMER_FOR_DELAY->SR & TIM_SR_UIF))
 		;
 }
-
 void DelayMs(uint16_t Delay_Ms) {
 	uint16_t tim_psc_val = 0;
 	tim_psc_val = SYSCLK / 1000 - 1;
@@ -29,7 +33,6 @@ void DelayMs(uint16_t Delay_Ms) {
 	while (!(TIMER_FOR_DELAY->SR & TIM_SR_UIF))
 		;
 }
-
 void DelayInit(void) {
 #ifdef TIM6
 	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
@@ -119,7 +122,6 @@ void PWM_SetDutyCycle(uint16_t Duty) {
 	TIMER_FOR_PWM->CCR1 = Duty;
 
 }
-
 void OnePwmPulseModeInit(uint16_t PulseLengthUs, uint16_t PulseCount) {
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	// Ініціалізація PA8, як канал 1 Таймера 1.
@@ -171,24 +173,102 @@ void OnePwmPulseModeInit(uint16_t PulseLengthUs, uint16_t PulseCount) {
 	 10: tDTS=4*tCK_INT
 	 11: Reserved, do not program this value
 	 Note: tDTS = 1/fDTS, tCK_INT = 1/fCK_INT.
-	*/
+	 */
 	TIM1->ARR = 1000;
-	TIM1->CCR1 = TIM1->ARR - PulseLengthUs;       // Довжина імпульса у мікросекундах
+	TIM1->CCR1 = TIM1->ARR - PulseLengthUs;  // Довжина імпульса у мікросекундах
 	TIM1->PSC = Prescaler; // Установка подільника тактування для таймера
 	TIM1->RCR = PulseCount - 1;     // Установка кількості повторів імпульсів
 	TIM1->EGR = TIM_EGR_UG; // Генерація події оновлення для негайної, перезагрузки PSC & RCR
 	TIM1->CR1 |= TIM_CR1_OPM; // Вибiр режимy One Pulse Mode:
-	TIM1->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_3 | TIM_CCMR1_OC1PE; // PWM Mode 1
+	TIM1->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2
+			| TIM_CCMR1_OC1M_3 | TIM_CCMR1_OC1PE; // PWM Mode 1
 	TIM1->SMCR = RESET;
 	TIM1->CR1 |= TIM_CR1_ARPE; // Autoreload Preload Enable
 	TIM1->CCER |= TIM_CCER_CC1E; // дозвіл виходу Compare каналу 1:
 	TIM1->BDTR |= TIM_BDTR_MOE; // Дозвіл основного виходу таймера:
 
-
 }
-
-void Pulse()
-{
+void Pulse() {
 	TIM1->CR1 |= TIM_CR1_CEN; // Дозвіл роботи таймера
 
 }
+
+void TIM1_PWM_DMA_BRUST_Init(void) {
+
+	/* TIM1 clock enable */
+	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+	/* DMA1 clock enable */
+	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+
+	/* GPIOA clock enable */
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+	/* Set CHSEL bits according to DMA Channel 5 */
+	/* Set DIR bits according to Memory to peripheral direction */
+	/* Set PINC bit according to DMA Peripheral Increment Disable */
+	/* Set MINC bit according to DMA Memory Increment Enable */
+	/* Set PSIZE bits according to Peripheral DataSize = Word */
+	/* Set MSIZE bits according to Memory DataSize Word */
+	/* Set CIRC bit according to circular mode */
+	/* Set PL bits according to very high priority */
+	/* Set MBURST bits according to single memory burst */
+	/* Set PBURST bits according to single peripheral burst */
+	DMA1_Channel5->CCR |= DMA_MEMORY_TO_PERIPH |
+	DMA_PINC_DISABLE | DMA_MINC_ENABLE |
+	DMA_PDATAALIGN_WORD | DMA_MDATAALIGN_WORD |
+	DMA_CIRCULAR | DMA_PRIORITY_HIGH;
+
+	/* Write to DMA1 Channel5 number of data register register */
+	DMA1_Channel5->CNDTR = BUFFER_DATA_NUMBER;
+
+	/* Write to DMA1 Channel5 peripheral address */
+	DMA1_Channel5->CPAR = (uint32_t) TIM1_DMAR_ADDRESS;
+
+	/* Write to DMA1 Channel5 Memory address register */
+	DMA1_Channel5->CMAR = (uint32_t) aSRC_Buffer;
+
+	/* Enable DMA1 Channel5 */
+	DMA1_Channel5->CCR |= (uint32_t) DMA_CCR_EN;
+
+	// Ініціалізація PA8, як канал 1 Таймера 1.
+	GPIOA->MODER |= GPIO_MODER_MODER8_1;
+	/*
+	 00: Input mode (reset state)
+	 01: General purpose output mode
+	 10: Alternate function mode !!!
+	 11: Analog mode
+	 */
+	GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR8;
+	/*
+	 x0: Low speed
+	 01: Medium speed
+	 11: High speed !!!
+	 */
+	GPIOA->AFR[1] |= 0x00000006; /* AF6: TIM1_CH1*/
+
+	Tim1Prescaler = (uint16_t) (SystemCoreClock / 32000000) - 1;
+	/* Configure the period */
+	TIM1->ARR = 0xFFFF;
+	TIM1->PSC = Tim1Prescaler; // Встановлюємо подільник частоти таймера на 1 МГц
+	TIM1->CCR1 = 0xFFF;
+	TIM1->CR1 &= ~ TIM_CR1_CKD; // Подільник частоти таймера також на 1
+	TIM1->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; // PWM Mode 1
+	TIM1->CCMR1 |= TIM_CCMR1_OC1PE; /* Enable the output compare 1 Preload */
+	TIM1->CR1 |= TIM_CR1_ARPE; /* Enable auto-reload Preload  */
+	TIM1->DIER |= TIM_DIER_UDE; /* Вмикаю оновлення таймера по DMA*/
+	TIM1->DCR &= ~TIM_DCR_DBA;
+	TIM1->DCR &= ~TIM_DCR_DBL;
+	TIM1->DCR = TIM_DMABase_ARR | TIM_DMABurstLength_3Transfers; // Кількість порцій за одну DMA транзакцію
+
+	TIM1->EGR |= TIM_EGR_UG; /* Обов'язково генеруємо подію Update */
+	/* Wait until the RESET of UG bit*/
+	while ((TIM1->EGR & TIM_EGR_UG) == SET)
+		;
+	/* Enable UEV by setting UG bit to load data from preload to active registers */
+	TIM1->EGR |= TIM_EGR_UG;
+	TIM1->BDTR |= TIM_BDTR_MOE; /*Вмикаю головний вихід TIM1*/
+	TIM1->CCER |= TIM_CCER_CC1E;/*Вмикаю перший канал*/
+	TIM1->CR1 |= TIM_CR1_CEN;
+}
+
--- Моя бібліотека роботи з таймерами/timer_cmsis/timer.h
+++ Моя бібліотека роботи з таймерами/timer_cmsis/timer.h
@@ -13,7 +13,10 @@
 
 #define SYSCLK 8000000UL     // Please set really timer clk freq
 
-
+#define TIM_DMABURSTLENGTH_3TRANSFERS   0x00000200U
+#define TIM_DMABASE_ARR   0x0000000BU
+#define TIM1_DMAR_ADDRESS ((uint32_t)0x40012C4C) /* TIM DMAR address for burst access*/
+#define BUFFER_DATA_NUMBER ((uint32_t)9)
 
 
 /* Функції для генерації затримок на таймері*/
@@ -30,5 +33,8 @@ void PWM_SetDutyCycle(uint16_t Duty);
 /* Функції для генерації одиночних PWM імпульсів */
 void OnePwmPulseModeInit(uint16_t PulseLengthUs, uint16_t PulseCount);
 void Pulse(); // Функція для генерування Заданої порції однакових імпульсів
+
+/* Функції для вивода шим-сигналу довільної форми з буфера aSRC_Buffer(timer.c) через DMA */
+void TIM1_PWM_DMA_BRUST_Init(void);
 
 #endif /* TIMER_H_ */
