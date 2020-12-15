bdc883a195e5e7410ffe03b8406b4a2fc73eefe3
Vitech-UA <viktorstarovit@gmail.com>
2020-12-15 22:54:37 +0200

    Додано підримку f3 серії

--- Моя бібліотека роботи з dma/cmsis_dma/cmsis_dma.c
+++ Моя бібліотека роботи з dma/cmsis_dma/cmsis_dma.c
@@ -7,20 +7,14 @@
 
 #include "cmsis_dma.h"
 
+#ifdef STM32F7
 void DMA_Init(DMA_Stream_TypeDef *Channel, uint32_t Src, uint32_t Dst,
 		uint32_t Size, uint32_t Conf)
 {
 
-#ifdef STM32F7
-
 	uint32_t tmp = 0;
-
-
 	DMA_Disable(Channel);
-
 	while((Channel->CR & DMA_SxCR_EN) != 0);
-
-
 	/* Обнуляю все */
 	tmp &= ((uint32_t)~(DMA_SxCR_CHSEL | DMA_SxCR_MBURST | DMA_SxCR_PBURST | \
 	                      DMA_SxCR_PL    | DMA_SxCR_MSIZE  | DMA_SxCR_PSIZE  | \
@@ -28,34 +22,21 @@ void DMA_Init(DMA_Stream_TypeDef *Channel, uint32_t Src, uint32_t Dst,
 	                      DMA_SxCR_DIR   | DMA_SxCR_CT     | DMA_SxCR_DBM));
 	Channel->CR = tmp;
 	tmp |= Conf;		   // Закатуємо на результат біти налаштувань
-
 	Channel->CR &= (uint32_t)(~DMA_SxCR_DBM);
-
 	Channel->NDTR = (uint16_t)Size;   // Number of data items to transfer (0 up to 65535)
-
 	Channel->PAR =  Src;  // Адреса периферії
 	Channel->M0AR = Dst;   // Адреса у пам'яті
 	Channel->CR = (uint32_t)tmp;	   // Записую налаштування у пам'ять
-
-
-
-#endif
 }
 
 void DMA_Enable(DMA_Stream_TypeDef *Channel)
 {
-#ifdef STM32F7
-
 	Channel->CR |= DMA_SxCR_EN; // Enable DMA
-#endif
 }
 
 void DMA_Disable(DMA_Stream_TypeDef *Channel)
 {
-#ifdef STM32F7
-
 	Channel->CR &= ~(uint32_t) (1<<0); // Enable DMA
-#endif
 }
 
 void DMA_DeInit(DMA_Stream_TypeDef* Channel)
@@ -65,7 +46,44 @@ void DMA_DeInit(DMA_Stream_TypeDef* Channel)
     Channel->NDTR = 0;
     Channel->M0AR = 0;
     Channel->M1AR = 0;
+}
+#endif
 
+
+void DMA_Init(DMA_Channel_TypeDef *Channel, uint32_t Src, uint32_t Dst,
+		uint32_t Size, uint16_t Conf)
+{
+
+	uint32_t tmp = 0;
+
+	    tmp = Channel->CCR;		// Копируем биты настройки
+	    tmp &= CCR_CLEAR_Mask;	// и стираем все кроме битов EN. А он и так будет 0
+	    tmp |= Conf;			// Закатываем на результат наши биты настроек.
+
+	    Channel->CNDTR = Size;	// Заполняем все нужные поля. Размер передчи
+	    Channel->CPAR = Src;	// Адрес периферии
+	    Channel->CMAR = Dst;		// Адрес в памяти
+	    Channel->CCR = tmp;		// Записываем настройки в память.
 }
 
+void DMA_Enable(DMA_Channel_TypeDef *Channel)
+{
+	Channel->CCR |= DMA_CCR_EN; // Enable DMA
+}
 
+void DMA_Disable(DMA_Channel_TypeDef *Channel)
+{
+	Channel->CCR &= ~(uint32_t) (1<<0); // Enable DMA
+}
+
+void DMA_DeInit(DMA_Channel_TypeDef* Channel)
+{
+    Channel->CCR &= (uint16_t)(~DMA_CCR_EN);
+    Channel->CCR = 0;
+    Channel->CNDTR = 0;
+    Channel->CPAR = 0;
+    Channel->CMAR = 0;
+
+
+
+}
--- Моя бібліотека роботи з dma/cmsis_dma/cmsis_dma.h
+++ Моя бібліотека роботи з dma/cmsis_dma/cmsis_dma.h
@@ -10,8 +10,8 @@
 
 #include <stdint.h>
 
-#define STM32F7
-//#define STM32F3
+//#define STM32F7
+#define STM32F3
 //#define STM32F1
 //#define STM32F0
 
@@ -34,6 +34,8 @@
 #ifdef STM32F0
 #include "stm32f0xx.h"
 #endif
+
+/* Справедливо для сімейства F7 */
 
 #ifdef STM32F7
 //0
@@ -125,6 +127,7 @@ In direct mode, these bits are forced to 0x0 by hardware */
 
 #endif
 
+#ifdef STM32F7
 void DMA_Init(DMA_Stream_TypeDef *Channel, uint32_t Src, uint32_t Dst,
 		uint32_t Size, uint32_t Conf);
 void DMA_Enable(DMA_Stream_TypeDef *Channel);
@@ -133,3 +136,59 @@ uint32_t DMA_GetCurrentDataCounter(DMA_Stream_TypeDef *Channel);
 void DMA_DeInit(DMA_Stream_TypeDef *Channel);
 void DMA1_Stream2_IRQHandler(void);
 #endif /* CMSIS_DMA_H_ */
+#endif
+
+
+/* Справедливо для сімейства F3 */
+
+#ifdef STM32F3
+#define DMAEnable ((uint16_t)(1<<0))
+#define DMADisable ((uint16_t)0)
+
+#define TransCompl_Int_Enable ((uint16_t)(1<<1))
+#define TransCompl_Int_Disable ((uint16_t)0)
+
+#define HalfCompl_Int_Enable ((uint16_t)1<<2)
+#define HalfCompl_Int_Disable ((uint16_t)0)
+
+#define TransError_Int_Enable ((uint16_t)(1<<3))
+#define TransError_Int_Disable ((uint16_t)0)
+
+#define ReadMemory ((uint16_t)(1<<4))
+#define ReadPerif ((uint16_t)0)
+
+#define CircularMode_Enable ((uint16_t)1<<5)
+#define CircularMode_Disable ((uint16_t)0)
+
+#define PeripheralInc_Enable ((uint16_t)(1<<6))
+#define PeripheralInc_Disable ((uint16_t)0)
+
+#define MemoryInc_Enable ((uint16_t)(1<<7))
+#define MemoryInc_Disable ((uint16_t)0)
+
+#define PDataSize_B ((uint16_t)0)
+#define PDataSize_W ((uint16_t)0x0100)
+#define PDataSize_DW ((uint16_t)0x0200)
+
+#define MDataSize_B ((uint16_t)0)
+#define MDataSize_W ((uint16_t)0x0400)
+#define MDataSize_DW ((uint16_t)0x0800)
+
+#define DMA_Priority_Low ((uint16_t)0)
+#define DMA_Priority_Med ((uint16_t)(1<<12))
+#define DMA_Priority_Hi  ((uint16_t)(2<<12))
+#define DMA_Priority_VHi ((uint16_t)(3<<12))
+
+#define M2M_Enable (1<<14)
+#define M2M_Disable 0
+#define CCR_CLEAR_Mask           ((uint32_t)0xFFFF8001)
+
+void DMA_Init(DMA_Channel_TypeDef *Channel, uint32_t Src, uint32_t Dst,
+		uint32_t Size, uint16_t Conf);
+void DMA_Enable(DMA_Channel_TypeDef *Channel);
+void DMA_Disable(DMA_Channel_TypeDef *Channel);
+uint32_t DMA_GetCurrentDataCounter(DMA_Channel_TypeDef *Channel);
+void DMA_DeInit(DMA_Channel_TypeDef *Channel);
+void DMA1_Stream2_IRQHandler(void);
+
+#endif
