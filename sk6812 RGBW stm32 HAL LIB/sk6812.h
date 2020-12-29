79efb6b1bb3e05c42745595bca7d935e358498b7
Vitech-UA <viktorstarovit@gmail.com>
2020-12-29 22:27:43 +0200

    Додано бібліотеку SK6812RGBW Adress RGB Led

---
+++ sk6812 RGBW stm32 HAL LIB/sk6812.h
@@ -0,0 +1,12 @@
+#ifndef _LED_DRIVER_SK6812
+#define _LED_DRIVER_SK6812
+
+#include <stdint.h>
+
+void led_set_RGB(uint8_t index, uint8_t r, uint8_t g, uint8_t b);
+void led_set_RGBW(uint8_t index, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+void led_set_all_RGB(uint8_t r, uint8_t g, uint8_t b);
+void led_set_all_RGBW(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+void led_render();
+
+#endif
