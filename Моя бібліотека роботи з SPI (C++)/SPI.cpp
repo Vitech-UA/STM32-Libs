b0d3e6739e83431c35742d40be9f246545cd15cc
Vitech-UA <viktorstarovit@gmail.com>
2021-02-14 12:35:13 +0200

    update gpio definition

--- Моя бібліотека роботи з SPI (C++)/SPI.cpp
+++ Моя бібліотека роботи з SPI (C++)/SPI.cpp
@@ -7,8 +7,38 @@
 
 #include <SPI.h>
 
-SPI::SPI() {
-	// TODO Auto-generated constructor stub
+SPI::SPI(SPI_TypeDef *Port, SPI_mode_t Mode) {
+	// TODO Допиляти цей конструктор
+	this->SPI_Item = Port;
+
+	switch (Mode) {
+	case Full_Duplex_Master:
+
+		break;
+	case Full_Duplex_Slave:
+
+		break;
+	case Half_Duplex_Master:
+
+		break;
+	case Half_Duplex_Slave:
+
+		break;
+	case Transmit_Only_Master:
+
+		break;
+	case Transmit_Only_Slave:
+
+		break;
+	case Receive_Only_Master:
+
+		break;
+	case Receive_Only_Slave:
+
+		break;
+	default:
+		break;
+	}
 
 }
 
--- Моя бібліотека роботи з SPI (C++)/SPI.h
+++ Моя бібліотека роботи з SPI (C++)/SPI.h
@@ -5,20 +5,69 @@
  *      Author: Embed Viktor
  */
 
-
-
 #ifndef SPI_H_
 #define SPI_H_
 
-#define STM32_SERIES 0
+typedef enum SPI_mode {
+	Full_Duplex_Master = 0,
+	Full_Duplex_Slave,
+	Half_Duplex_Master,
+	Half_Duplex_Slave,
+	Receive_Only_Master,
+	Receive_Only_Slave,
+	Transmit_Only_Master,
+	Transmit_Only_Slave
+} SPI_mode_t;
+
+ #define STM32_SERIES 0
 // #define STM32_SERIES 1
 // #define STM32_SERIES 3
 // #define STM32_SERIES 4
 
-//#define USE_HARDWARE_nCS // Розкоментувати для апаратного контролю піном nCS, закоментувати для програмного
-
 #if (STM32_SERIES == 0)
 
+	#define SPI1_USE_HARDWARE_In_nCS
+    #define SPI1_USE_HARDWARE_Out_nCS
+
+    #define SPI2_USE_HARDWARE_In_nCS
+	#define SPI2_USE_HARDWARE_Out_nCS
+
+	// SPI1 GPIO Definition //
+	#define SPI1_SCK_PORT PA5
+	//#define SPI1_SCK_PORT PB3
+	#define SPI1_MISO_PORT PA6
+	//#define SPI1_MISO_PORT PB4
+	#define SPI1_MOSI_PORT PA7
+	//#define SPI1_MOSI_PORT PB5
+
+	#ifdef SPI1_USE_HARDWARE_Out_nCS
+		#define SPI1_nSS_PORT PA4
+		//#define SPI1_nSS_PORT PA15
+	#endif
+
+	#ifdef SPI1_USE_HARDWARE_In_nCS
+		#define SPI1_nSS_PORT PA4
+		//#define SPI1_nSS_PORT PA15
+	#endif
+
+
+	// SPI2 GPIO Definition //
+	#define SPI2_SCK_PORT PA5
+	//#define SPI2_SCK_PORT PB3
+	#define SPI2_MISO_PORT PA6
+	//#define SPI2_MISO_PORT PB4
+	#define SPI2_MOSI_PORT PA7
+	//#define SPI2_MOSI_PORT PB5
+
+    #ifdef SPI2_USE_HARDWARE_Out_nCS
+		#define SPI2_nSS_PORT PA4
+		//#define SPI2_nSS_PORT PA15
+	#endif
+
+	#ifdef SPI2_USE_HARDWARE_In_nCS
+		#define SPI2_nSS_PORT PA4
+		//#define SPI2_nSS_PORT PA15
+	#endif
 #endif
 
 #if (STM32_SERIES == 1)
@@ -33,9 +82,17 @@
 
 #endif
 
+#if (STM32_SERIES != 0 || STM32_SERIES != 1 || STM32_SERIES != 3 || STM32_SERIES != 4)
+// #error Please uncoment STM32_SERIES in SPI.h
+#endif
+
 class SPI {
 public:
-	SPI();
+	SPI(SPI_TypeDef *Port, SPI_mode_t Mode);
+	void Init();
+private:
+	SPI_TypeDef *SPI_Item;
+
 };
 
 #endif /* SPI_H_ */
