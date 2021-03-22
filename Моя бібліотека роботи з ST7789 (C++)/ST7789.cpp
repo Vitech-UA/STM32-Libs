7e9293b1abbd553050e58f0afbce03602b671a98
Vitech-UA <viktorstarovit@gmail.com>
2021-03-22 22:35:02 +0200

    Прописав ініціалізацію LCD, додав дефайни, описав службові ф-ї

--- Моя бібліотека роботи з ST7789 (C++)/ST7789.cpp
+++ Моя бібліотека роботи з ST7789 (C++)/ST7789.cpp
@@ -7,17 +7,52 @@
 
 #include "ST7789.h"
 
+/*
+ Розпіновка тестової плати
+ MOSI - PA7
+ SCK - PA5
+ RES - PC5
+ DC - PC4
+ BLK - PA4
+ */
+
 ST7789::ST7789(SPI_TypeDef *Port, SPI_DataSize_t size, GPIO_TypeDef *BlinkPort,
-		uint16_t BlinkPin) :
+		uint16_t BlinkPin, GPIO_TypeDef *ResetPort, uint16_t ResetPin,
+		GPIO_TypeDef *DcPort, uint16_t DcPin) :
 		SPI(Port, size) {
-	this->ItemBlinkPin = BlinkPin;
+
 	this->ItemBlinkPort = BlinkPort;
+	this->ItemBlinkPin = BlinkPin;
 	this->Blink = Gpio(this->ItemBlinkPort, this->ItemBlinkPin);
 	this->Blink.SetAsGenerapPurporseOutput(OUTPUT_PP);
 
+	this->ItemResetPort = ResetPort;
+	this->ItemResetPin = ResetPin;
+	this->Reset = Gpio(this->ItemResetPort, this->ItemResetPin);
+	this->Reset.SetAsGenerapPurporseOutput(OUTPUT_PP);
+
+	this->ItemDcPort = DcPort;
+	this->ItemDcPin = DcPin;
+	this->Dc = Gpio(this->ItemDcPort, this->ItemDcPin);
+	this->Dc.SetAsGenerapPurporseOutput(OUTPUT_PP);
+
+	this->ST7789_Width = Width;
+	this->ST7789_Height = Height;
+	this->HardReset();
+	this->SoftReset();
+	this->SleepModeExit();
+	this->ColorModeSet(ST7789_ColorMode_65K | ST7789_ColorMode_16bit);
+	delayMs(10);
+	this->MemAccessModeSet(4, 1, 1, 0);
+	delayMs(10);
+	this->InversionMode(1);
+	delayMs(10);
+	this->FillScreen(GREEN);
+    this->SetBL(10);
+    this->DisplayPower(1);
+    delayMs(10);
+
 }
-
-
 
 void ST7789::BrightnessEnable(void) {
 	this->Blink.Set();
@@ -25,4 +60,176 @@ void ST7789::BrightnessEnable(void) {
 void ST7789::BrightnessDisable(void) {
 	this->Blink.Reset();
 
+}
+
+void ST7789::Write(uint8_t Data) {
+
+	this->TransmitBlocking(Data);
+}
+
+void ST7789::HardReset(void) {
+	this->Reset.Reset(); // ResetPin low
+	delayMs(10);
+	this->Reset.Set();
+	delayMs(150);
+
+}
+void ST7789::SoftReset(void) {
+	this->WriteCMD(ST7789_Cmd_SWRESET);
+	delayMs(130);
+}
+
+void ST7789::SendData(uint8_t data) {
+	this->Dc.Set();
+	*((__IO uint8_t*) &SPI1->DR) = data;
+}
+
+void ST7789::SleepModeExit(void) {
+	this->WriteCMD(ST7789_Cmd_SLPOUT);
+	delayMs(500);
+}
+
+void ST7789::ColorModeSet(uint8_t ColorMode) {
+	this->WriteCMD(ST7789_Cmd_COLMOD);
+	this->SendData(ColorMode & 0x77);
+}
+
+void ST7789::WriteCMD(uint8_t CMD) {
+	this->DC_Command();
+	this->Write(CMD);
+}
+
+void ST7789::MemAccessModeSet(uint8_t Rotation, uint8_t VertMirror,
+		uint8_t HorizMirror, uint8_t IsBGR) {
+	uint8_t Value;
+	Rotation &= 7;
+
+	this->WriteCMD(ST7789_Cmd_MADCTL);
+
+	switch (Rotation) {
+	case 0:
+		Value = 0;
+		break;
+	case 1:
+		Value = ST7789_MADCTL_MX;
+		break;
+	case 2:
+		Value = ST7789_MADCTL_MY;
+		break;
+	case 3:
+		Value = ST7789_MADCTL_MX | ST7789_MADCTL_MY;
+		break;
+	case 4:
+		Value = ST7789_MADCTL_MV;
+		break;
+	case 5:
+		Value = ST7789_MADCTL_MV | ST7789_MADCTL_MX;
+		break;
+	case 6:
+		Value = ST7789_MADCTL_MV | ST7789_MADCTL_MY;
+		break;
+	case 7:
+		Value = ST7789_MADCTL_MV | ST7789_MADCTL_MX | ST7789_MADCTL_MY;
+		break;
+	}
+
+	if (VertMirror)
+		Value = ST7789_MADCTL_ML;
+	if (HorizMirror)
+		Value = ST7789_MADCTL_MH;
+
+	if (IsBGR)
+		Value |= ST7789_MADCTL_BGR;
+
+	this->SendData(Value);
+}
+
+void ST7789::InversionMode(uint8_t Mode) {
+	if (Mode)
+		this->WriteCMD(ST7789_Cmd_INVON);
+	else
+		this->WriteCMD(ST7789_Cmd_INVOFF);
+}
+
+void ST7789::ResetLow(void) {
+	this->Reset.Reset();
+}
+void ST7789::ResetHigh(void) {
+	this->Reset.Set();
+}
+void ST7789::DC_Data(void) {
+	this->Dc.Set();
+}
+void ST7789::DC_Command(void) {
+	this->Dc.Reset();
+}
+
+void ST7789::FillScreen(uint16_t color) {
+	this->FillRect(0, 0, ST7789_Width, ST7789_Height, color);
+}
+void ST7789::FillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color) {
+	if ((x >= ST7789_Width) || (y >= ST7789_Height))
+		return;
+	if ((x + w) > ST7789_Width)
+		w = ST7789_Width - x;
+	if ((y + h) > ST7789_Height)
+		h = ST7789_Height - y;
+	this->SetWindow(x, y, x + w - 1, y + h - 1);
+	for (uint32_t i = 0; i < (h * w); i++)
+		this->RamWrite(&color, 1);
+}
+void ST7789::SetWindow(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1) {
+	this->ColumnSet(x0, x1);
+	this->RowSet(y0, y1);
+	this->WriteCMD(ST7789_Cmd_RAMWR);
+}
+
+void ST7789::RamWrite(uint16_t *pBuff, uint16_t Len) {
+	while (Len--) {
+		this->SendData(*pBuff >> 8);
+		this->SendData(*pBuff & 0xFF);
+	}
+}
+void ST7789::ColumnSet(uint16_t ColumnStart, uint16_t ColumnEnd) {
+	if (ColumnStart > ColumnEnd)
+		return;
+	if (ColumnEnd > ST7789_Width)
+		return;
+
+	ColumnStart += ST7789_X_Start;
+	ColumnEnd += ST7789_X_Start;
+
+	this->WriteCMD(ST7789_Cmd_CASET);
+	this->SendData(ColumnStart >> 8);
+	this->SendData(ColumnStart & 0xFF);
+	this->SendData(ColumnEnd >> 8);
+	this->SendData(ColumnEnd & 0xFF);
+}
+
+void ST7789::RowSet(uint16_t RowStart, uint16_t RowEnd) {
+	if (RowStart > RowEnd)
+		return;
+	if (RowEnd > ST7789_Height)
+		return;
+
+	RowStart += ST7789_Y_Start;
+	RowEnd += ST7789_Y_Start;
+
+	this->WriteCMD(ST7789_Cmd_RASET);
+	this->SendData(RowStart >> 8);
+	this->SendData(RowStart & 0xFF);
+	this->SendData(RowEnd >> 8);
+	this->SendData(RowEnd & 0xFF);
+}
+
+void ST7789::SetBL(uint8_t Value) {
+	if (Value > 100)
+		Value = 100;
+}
+
+void ST7789::DisplayPower(uint8_t On) {
+	if (On)
+		this->WriteCMD(ST7789_Cmd_DISPON);
+	else
+		this->WriteCMD(ST7789_Cmd_DISPOFF);
 }
--- Моя бібліотека роботи з ST7789 (C++)/ST7789.h
+++ Моя бібліотека роботи з ST7789 (C++)/ST7789.h
@@ -9,17 +9,104 @@
 #define ST7789_H_
 
 #include "SPI.h"
+#include "delay.h"
+
+#define ST7789_ColorMode_65K    0x50
+#define ST7789_ColorMode_262K   0x60
+#define ST7789_ColorMode_12bit  0x03
+#define ST7789_ColorMode_16bit  0x05
+#define ST7789_ColorMode_18bit  0x06
+#define ST7789_ColorMode_16M    0x07
+
+#define ST7789_Cmd_SWRESET      0x01
+#define ST7789_Cmd_SLPIN        0x10
+#define ST7789_Cmd_SLPOUT       0x11
+#define ST7789_Cmd_COLMOD       0x3A
+#define ST7789_Cmd_PTLON        0x12
+#define ST7789_Cmd_NORON        0x13
+#define ST7789_Cmd_INVOFF       0x20
+#define ST7789_Cmd_INVON        0x21
+#define ST7789_Cmd_GAMSET       0x26
+#define ST7789_Cmd_DISPOFF      0x28
+#define ST7789_Cmd_DISPON       0x29
+#define ST7789_Cmd_CASET        0x2A
+#define ST7789_Cmd_RASET        0x2B
+#define ST7789_Cmd_RAMWR        0x2C
+#define ST7789_Cmd_PTLAR        0x30
+#define ST7789_Cmd_MADCTL       0x36
+#define ST7789_Cmd_MADCTL_MY    0x80
+#define ST7789_Cmd_MADCTL_MX    0x40
+#define ST7789_Cmd_MADCTL_MV    0x20
+#define ST7789_Cmd_MADCTL_ML    0x10
+#define ST7789_Cmd_MADCTL_RGB   0x00
+#define ST7789_Cmd_RDID1        0xDA
+#define ST7789_Cmd_RDID2        0xDB
+#define ST7789_Cmd_RDID3        0xDC
+#define ST7789_Cmd_RDID4        0xDD
+#define ST7789_MADCTL_MY        0x80
+#define ST7789_MADCTL_MX        0x40
+#define ST7789_MADCTL_MV        0x20
+#define ST7789_MADCTL_ML        0x10
+#define ST7789_MADCTL_BGR       0x08
+#define ST7789_MADCTL_MH        0x04
+
+#define BLACK    0x0000
+#define BLUE     0x001F
+#define RED      0xF800
+#define GREEN    0x07E0
+#define CYAN     0x07FF
+#define MAGENTA  0xF81F
+#define YELLOW   0xFFE0
+#define WHITE    0xFFFF
+
+#define Width 240
+#define Height 240
+
+#define ST7789_X_Start          0
+#define ST7789_Y_Start          0
 
 class ST7789: public SPI {
 public:
 	ST7789(SPI_TypeDef *Port, SPI_DataSize_t size, GPIO_TypeDef *BlinkPort,
-			uint16_t BlinkPin);
+			uint16_t BlinkPin, GPIO_TypeDef *ResetPort, uint16_t ResetPin,
+			GPIO_TypeDef *DcPort, uint16_t DcPin);
 	void BrightnessEnable(void);
 	void BrightnessDisable(void);
+	void FillScreen(uint16_t color);
 private:
+	void ResetLow(void);
+	void ResetHigh(void);
+	void DC_Data(void);
+	void DC_Command(void);
+	void Write(uint8_t Data);
+	void WriteCMD(uint8_t CMD);
+	void HardReset(void);
+	void SoftReset(void);
+	void SendData(uint8_t data);
+	void SleepModeExit(void);
+	void ColorModeSet(uint8_t ColorMode);
+	void MemAccessModeSet(uint8_t Rotation, uint8_t VertMirror,
+			uint8_t HorizMirror, uint8_t IsBGR);
+	void InversionMode(uint8_t Mode);
+
+	void FillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+	void SetWindow(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+	void RamWrite(uint16_t *pBuff, uint16_t Len);
+	void ColumnSet(uint16_t ColumnStart, uint16_t ColumnEnd);
+	void RowSet(uint16_t RowStart, uint16_t RowEnd);
+	void SetBL(uint8_t Value);
+	void DisplayPower(uint8_t On);
 	GPIO_TypeDef *ItemBlinkPort;
 	uint16_t ItemBlinkPin;
+	GPIO_TypeDef *ItemResetPort;
+	uint16_t ItemResetPin;
+	GPIO_TypeDef *ItemDcPort;
+	uint16_t ItemDcPin;
 	Gpio Blink;
+	Gpio Reset;
+	Gpio Dc;
+	uint8_t ST7789_Width;
+	uint8_t ST7789_Height;
 };
 
 #endif /* ST7789_H_ */
